4fa43c2ed1bcf3c09d30a869b4928b6a3fff83d0
Al Mamun Bin Mozid <al.mamun@bjitgroup.com>
2024-08-29 09:32:06 +0600

    First commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by BJIT LTD on 29/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
